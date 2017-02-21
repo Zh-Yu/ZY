7a061b6beeb461aeef5da2e5dda7886f604b52e7
ZH-Yu <13207507632@163.com>
2017-02-21 16:41:27 +0800

    20170221 16:41

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -2,7 +2,7 @@
 <div class="goods">
   <div class="menu-wrapper" v-el:menu-wrapper>
   	<ul>
-  		<li v-for="item in goods" class="menu-item">
+  		<li v-for="item in goods" class="menu-item" :class="{'current':currentIndex===$index}">
   		  <span class="text border-1px">
   		  	<span v-show="item.type>0" class="icon" :class="classmap[item.type]"></span>{{item.name}}
   		  </span> 			
@@ -11,7 +11,7 @@
   </div>
   <div class="foods-wrapper" v-el:foods-wrapper>
   	<ul>
-  		<li v-for="item in goods" class="food-list">
+  		<li v-for="item in goods" class="food-list food-list-hock">
   			<h1 class="title">{{item.name}}</h1>
   			<ul>
   				<li v-for="food in item.foods" class="food-item  border-1px">
@@ -48,8 +48,22 @@ import BScroll from 'better-scroll';
    	},
    	data(){
    	  return{
-   	    goods:[]
+   	    goods:[],
+   	    listHeight:[],
+   	    scrollY: 0
    	  }
+   	},
+   	computed:{
+      currentIndex(){
+        for(let i=0; i<this.listHeight.length;i++){
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if(!height2 || (this.scrollY > height1 && this.scrollY < height2)){
+            return i;
+          }
+        }
+        return 0;
+      }
    	},
    	created(){
       this.classmap = ['decrease','discount','special','invoice','guarantee'];
@@ -59,6 +73,7 @@ import BScroll from 'better-scroll';
          this.goods = response.data;
          this.$nextTick(() => {
             this._initScroll();
+            this._calculateHeight();
          }); 
        }
    	  });
@@ -66,8 +81,23 @@ import BScroll from 'better-scroll';
    	methods: {
    	_initScroll(){
        this.menuScroll = new BScroll(this.$els.menuWrapper,{});
+       this.foodsScroll = new BScroll(this.$els.foodsWrapper,{
+         probeType: 3
+       });
 
-       this.foodsScroll = new BScroll(this.$els.foodsWrapper,{});
+    this.foodsScroll.on('scroll',(pos) => {
+      this.scrollY = Math.abs(Math.round(pos.y));
+    });
+   	},
+   	_calculateHeight(){
+      let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hock');
+      let height = 0;
+      this.listHeight.push(height);
+      for(let i=0;i<foodList.length;i++){
+        let item = foodList[i];
+        height +=item.clientHeight;
+        this.listHeight.push('height');
+      }
    	}
    	}
    };
@@ -92,6 +122,14 @@ import BScroll from 'better-scroll';
       width: 56px
       line-height:14px
       padding: 0 12px
+      &.current
+        font-weight: 700
+        position: relative
+        z-index: 10
+        background: #fff
+        margin-top: -1px
+        .text
+          border-none() 
       .icon
         vertical-align: top
         height: 12px
@@ -167,11 +205,4 @@ import BScroll from 'better-scroll';
             text-decoration: line-through
             font-size: 10px
             color: rgb(147,153,159)
-
-
-
-        
-
-
-
 </style>
