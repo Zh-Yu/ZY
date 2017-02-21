3ac9841ed0a8b305f34d7f1d1ca051ef41edf2b2
ZH-Yu <13207507632@163.com>
2017-02-21 11:50:48 +0800

    20170221 星期二 中午

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="goods">
-  <div class="menu-wrapper">
+  <div class="menu-wrapper" v-el:menu-wrapper>
   	<ul>
   		<li v-for="item in goods" class="menu-item">
   		  <span class="text border-1px">
@@ -9,7 +9,7 @@
   		</li>
   	</ul>
   </div>
-  <div class="foods-wrapper">
+  <div class="foods-wrapper" v-el:foods-wrapper>
   	<ul>
   		<li v-for="item in goods" class="food-list">
   			<h1 class="title">{{item.name}}</h1>
@@ -22,12 +22,10 @@
   					<h2 class="name">{{food.name}}</h2>
   					<p class="desc">{{food.description}}</p>
   					<div class="extra">
-  						<span class="count">月售{{food.sellCount}}份</span>
-  						<span>好评率{{food.rating}}%<span>
+  						<span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%<span>
   					</div>
   					<div class="price">
-  						<span>¥{{food.price}}</span>
-  						<span v-show="food.oldPrice">￥{{food.olePrice}}</span>
+  						<span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
   					</div>
   				</div>  					
   				</li>
@@ -39,6 +37,8 @@
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll';
+
   const ERR_OK = 0;
    export default{
    	props:{
@@ -57,8 +57,18 @@
        response = response.body;
        if(response.errno == ERR_OK){
          this.goods = response.data;
+         this.$nextTick(() => {
+            this._initScroll();
+         }); 
        }
-   	  })
+   	  });
+   	},
+   	methods: {
+   	_initScroll(){
+       this.menuScroll = new BScroll(this.$els.menuWrapper,{});
+
+       this.foodsScroll = new BScroll(this.$els.foodsWrapper,{});
+   	}
    	}
    };
 </script>
@@ -116,7 +126,7 @@
       border-left: 2px solid #d9dde1
       font-size: 12px
       color: rgb(147,153,159)
-      background: f3f5f7
+      background: #f3f5f7
     .food-item
       display: flex
       margin: 18px
@@ -138,10 +148,28 @@
           color:rgb(7,17,27)
         .desc,.extra
           font-size: 10px
-          line-height: 10px
           color: rgb(147,153,159)
         .desc
           margin-bottom: 8px
+          line-height: 14px        
+        .extra
+          line-height: 10px   
+          .count
+            margin-right: 12px
+        .price
+          font-weight: 700
+          line-height: 24px
+          .now
+            margin-right: 8px
+            font-size: 14px
+            color: rgb(240,20,20)
+          .old
+            text-decoration: line-through
+            font-size: 10px
+            color: rgb(147,153,159)
+
+
+
         
 
 
