bf4ffb8fedd2b58e62bbed7a2ded87b0261e069e
Zh-Yu <13207507632@163.com>
2017-03-07 22:01:46 +0800

    20170307 晚上 7-13shopcart购物车组件

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -6,11 +6,14 @@
   				<div class="logo">
   					<i class="icon-shopping_cart"></i>
   				</div>
+          <div class="num">{{totalCount}}</div>
   			</div>
-  				<div class="price">0</div>
+  				<div class="price">¥{{totalPrice}}</div>
   				<div class="desc">另需配送费¥{{deliveryPrice}}元</div>
   		</div>
-  		<div class="content-right"></div>
+  		<div class="content-right">
+        <div class="pay">¥{{minPrice}}元起送</div>      
+      </div>
   	</div>
   </div>
 </template>
@@ -18,6 +21,12 @@
 <script type="text/ecmascript-6">
    export default{
      props:{
+       selectFoods:{
+         type:Array,
+         default(){
+           return[];
+         }
+       },
        deliveryPrice:{
          type: Number,
          default: 0
@@ -25,6 +34,22 @@
        minPrice:{
          type: Number,
          default: 0
+       }
+     },
+     computed:{
+       totalPrice(){
+         let total = 0;
+         this.selectFoods.forEach((food) =>{
+           total += food.price * food.count;
+         });
+         return total;
+       },
+       totalCount(){
+         let count= 0;
+         this.selectFoods.forEach((food) =>{
+           count += food.count;
+         });
+         return count;
        }
      }
    };
@@ -42,6 +67,7 @@
     display: flex
     background: #141d27
     font-size: 0
+    color: rgba(255,255,255,0.4)
     .content-left
       flex: 1
       .logo-wrapper
@@ -77,16 +103,22 @@
         border-right: 1px solid rgba(255,255,255,0.1)
         font-size: 16px
         font-weight: 700
-        color: rgba(255,255,255,0.4)
       .desc
         display: inline-block
         vertical-align: top
         line-height: 24px
         margin: 12px 0 0 12px
         font-size: 10px
-        color: rgba(255,255,255,0.4)
         font-weight: 200
     .content-right
       flex: 0 0 105px
       width: 105px  
+      .pay
+        height: 48px
+        line-height: 48px
+        text-align: center
+        font-size: 12px
+        font-weight: 700
+        background:#2b333b
+        
 </style>
