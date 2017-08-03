63d91cfd528e3fbdc8b2f9ef42a01993930af38d
暑假数据库 <13207507623@163.com>
2017-08-03 13:25:03 +0800

    六个button

--- src/components/homepage/component/name.vue
+++ src/components/homepage/component/name.vue
@@ -2,34 +2,42 @@
 <div class="name">
 	<div class="inputname">
 		<i-input placeholder="请输入姓名..." :value.sync="name">
-	        <i-button slot="append" icon="ios-arrow-forward" :class="{ 'isClicked': isclicked}" @click="SearchByName">搜索</i-button>
+	        <i-button slot="append" icon="ios-arrow-forward"  :disabled="isClicked"  @click="SearchByName">搜索</i-button>
 	        <!-- @click.stop="clickButton" -->
 	    </i-input>
     </div>
-	    <Row>
-	        <i-col span="14">
-	            <Card>		            	
-            		<p slot="title">姓名：{{namequeryresult.name}}</p>
-                	<p class="extra" slot="extra">性别：{{namequeryresult.sex}}</p>            
-	                <p class="des texthight"> 描述：{{namequeryresult.location + namequeryresult.type}}</p>
-	                <p class="des">病例图<i-button class="btn1" size="small">弹出</i-button></p>
-	                <p class="des">CT图<i-button class="btn1" size="small">弹出</i-button></p>
-	                <p class="left">阻抗数据</p>
-	                <div class="right">
-	                	<p>
-                		<i-button class="btnbase" size="small">癌症实部</i-button>
-                		<i-button class="btnbase" size="small">正常实部</i-button>
-                		<i-button class="btnbase" size="small">癌症-正常实部</i-button>
-                		</p>
-                		<p>
-                		<i-button class="btnbase btnbottom" size="small">癌症虚部</i-button>
-                		<i-button class="btnbase btnbottom" size="small">正常虚部</i-button>
-                		<i-button class="btnbase btnbottom" size="small">癌症-正常虚部</i-button>	
-                	</p>
-	                </div>            
-	            </Card>
-	        </i-col>
-        </Row>
+    <Row>
+        <i-col span="14">
+            <Card>		            	
+        		<p slot="title">姓名：{{NameQueryResult.name}}</p>
+            	<p class="extra" slot="extra">性别：{{NameQueryResult.sex}}</p>            
+                <p class="des texthight"> 描述：{{NameQueryResult.location + NameQueryResult.type}}</p>
+                <p class="des">病例图<i-button class="btn1" size="small">弹出</i-button></p>
+                <p class="des">CT图<i-button class="btn1" size="small">弹出</i-button></p>
+                <p class="left">阻抗数据</p>
+                <div class="right" @click="select">
+                	<p>
+            		<i-button class="btnbase" size="small" @click="selectButton.disreal = true">癌症实部</i-button>
+            		<i-button class="btnbase" size="small" @click="selectButton.norreal = true">正常实部</i-button>
+            		<i-button class="btnbase" size="small" @click="selectButton.diffreal = true">癌症-正常实部</i-button>
+            		</p>
+            		<p>
+            		<i-button class="btnbase btnbottom" size="small" @click="selectButton.disimag = true">癌症虚部</i-button>
+            		<i-button class="btnbase btnbottom" size="small" @click="selectButton.norimag = true">正常虚部</i-button>
+            		<i-button class="btnbase btnbottom" size="small" @click="selectButton.diffimag = true">癌症-正常虚部</i-button>	
+            	</p>
+                </div>            
+            </Card>
+        </i-col>
+    </Row>
+    <Modal
+        title="阻抗数据"
+        :visible.sync= "selectTrue"
+        @on-ok="ok"
+        @on-cancel="cancel"
+        class-name="vertical-center-modal">
+        <div class="content">{{electinfo}}</div>
+	</Modal>
 </div>
 </template>
 <script>
@@ -38,12 +46,19 @@ import axios from 'axios';
         data(){
         	return{
         		name: "",
-        		namequeryresult: {					
-					sex: "",
-					type: "",
-					location: ""
+        		NameQueryResult: {},
+        		isClicked:false,
+        		selectButton:{
+        			disreal:false,
+        			norreal:false,
+        			diffreal:false,
+        			disimag:false,
+        			norimag:false,
+        			diffimag:false
         		},
-        		isclicked:false
+        		titletextdesc:["癌症实部","癌症虚部","癌症-正常实部","癌症虚部","正常虚部","癌症-正常虚部"],
+        		electinfo:"",
+        		selectTrue: "",
         	}
 	    },
 	    methods: {
@@ -52,34 +67,63 @@ import axios from 'axios';
 	          	// var url = "http://localhost:3000/patient?name=白建华"; // 创建script标签，设置其属性
 	          	//  var script = document.createElement('script'); script.setAttribute('src', url); // 把script标签加入head，此时调用开始
 	          	//  document.getElementsByTagName('head')[0].appendChild(script);
-	          	if(this.isclicked) return;
-	          	this.isclicked = true;
+	          	if(this.isClicked) return;
+	          	this.isClicked = true;
           		var url = 'http://localhost:3000/patient?name=' + this.name;
             	axios.get(url)		            
 			    .then(response =>{
-			      	this.namequeryresult = response.data.namequery[0];
-			      	if(this.namequeryresult.sex == 0)
-			      		this.namequeryresult.sex ='男';
+			      	this.NameQueryResult = response.data.namequery[0];
+			      	if(this.NameQueryResult.sex == 0)
+			      		this.NameQueryResult.sex ='男';
 			      	else
-			      		this.namequeryresult.sex ='女';
-			      	this.isclicked = false;
+			      		this.NameQueryResult.sex ='女';
+			      	this.isClicked = false;
 			      	
 			    })
-			    .catch(function(err){
+			    .catch(err =>{
 			      	alert("啥子玩意你找的是");
-			      	this.isclicked = false;
-			    });			    	          	
-	        }  
-	    }
+			      	this.isClicked = false;
+			    });			    	          
+	        },	        
+            select(){
+            	for(var key in this.selectButton){
+            		if (this.selectButton[key]){            			
+        				this.selectTrue = this.selectButton[key];           			
+        				// this.electinfo = this.NameQueryResult[key].split(",");
+        				var temparry = JSON.parse(this.NameQueryResult[key]);
+        				for (var i = 0; i < 31; i++) {
+        					this.electinfo += temparry[i] + " ;";
+        					if (i>0 && i%6 == 0)
+        						this.electinfo += "\n"
+        				}
+        				// this.titletext = this.titletextdesc[index] + "阻抗数据"        			
+            		}　
+            	}
+            },
+            ok(){
+            	for(var key in this.selectButton){
+            		if (this.selectButton[key]){
+            			   this.selectButton[key] = false;       			
+            		}
+            	}
+            	this.electinfo ="";
+            },
+            cancel(){
+            	for(var key in this.selectButton){
+            		if (this.selectButton[key]){
+            			   this.selectButton[key] =false;       			
+            		}
+            	}
+            	this.electinfo ="";
+            }
+        } 
+	   
     }
 </script>
 
 <style type="text/css">
 a{
 	color:black;
-}
-.isClicked{
-	color: red;
 }
 .inputname {   
     width: 30%;
@@ -108,5 +152,8 @@ a{
 }
 .right{
 	margin-left: 70px;
+}
+.content{
+	height: 200px;
 }
 </style>
